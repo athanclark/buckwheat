7dd4443abc4f5a4ad4b5b180693089891cabcfee
athanclark <athan.clark@gmail.com>
2013-10-30 12:47:06 -0600

    Redesigned presentation strategy

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 Modernizr.load({
 	test: Modernizr.svg,
-	yep: ['js/raphael-min.js', 'js/svg-insert.js']
+	yep: ['js/svg-insert.js'],
 	nope: 'js/img-insert.js'
 });
---
+++ js/svg-insert.js
@@ -0,0 +1,3 @@
+$('window').ready(function() {
+	$('#content').load('buckwheat-min.svg');
+});
